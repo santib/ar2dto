df899715d5527530de22978a754cff97d699c9b3
Santiago Bartesaghi <santib@hey.com>
2022-06-02 20:36:16 -0300

    Initial commit

---
+++ lib/ar2dto.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ar2dto/version"
+
+module Ar2dto
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ar2dto/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ar2dto
+  VERSION = "0.1.0"
+end
