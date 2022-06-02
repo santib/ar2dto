bb9b74ed5d5f3cd3a13458b04a6b2f8083587a1f
Santiago Bartesaghi <santib@hey.com>
2022-06-02 20:55:31 -0300

    First version of implementation

---
+++ lib/ar2dto/base.rb
@@ -0,0 +1,26 @@
+module AR2DTO
+  module Base
+    def self.included(model)
+      namespace = model.name.deconstantize.presence&.constantize || Object
+
+      namespace.const_set("#{model.name.split('::').last}DTO", Class.new do
+        include ::ActiveModel::Model
+        attr_reader :attributes
+        attr_accessor *model.column_names
+
+        def initialize(attributes)
+          @attributes = attributes
+          super
+        end
+
+        def ==(other)
+          attributes == other.attributes
+        end
+      end)
+    end
+
+    def to_dto
+      "#{self.class.name}DTO".constantize.new(attributes)
+    end
+  end
+end
