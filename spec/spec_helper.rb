9d7914073cbf45fede9b7dee712eaf2b615ea2a2
Santiago Bartesaghi <santib@hey.com>
2022-06-14 20:57:31 -0300

    Add tests for namespaced model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,9 @@ ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 
 require "ar2dto"
 
-load "#{File.dirname(__FILE__)}/support/schema.rb"
+require "support/schema"
+require "support/fixtures/user"
+require "support/fixtures/shop/order"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
