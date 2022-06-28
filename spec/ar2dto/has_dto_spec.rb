b603b699c7d6cb5f6f8c7f2d490ff2df01cee809
Santiago Bartesaghi <santib@hey.com>
2022-06-28 17:06:29 -0300

    Fix respecting a class when it already exists (#23)

--- spec/ar2dto/has_dto_spec.rb
+++ spec/ar2dto/has_dto_spec.rb
@@ -2,17 +2,23 @@
 
 RSpec.describe ".has_dto" do
   it "creates a new DTO class" do
+    User # autoload class
+
     expect(Object.const_defined?("UserDTO")).to be true
 
     expect(UserDTO.superclass).to eq AR2DTO::DTO
   end
 
   it "creates the new DTO class in the correct namespace" do
+    Shop::Order # autoload class
+
     expect(Shop.const_defined?("OrderDTO")).to be true
   end
 
   context "when the class already exists" do
     it "does not create a new class" do
+      Person # autoload class
+
       expect(Object.const_defined?("PersonDTO")).to be true
 
       expect(PersonDTO.superclass).to_not eq AR2DTO::DTO
