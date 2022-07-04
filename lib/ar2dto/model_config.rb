9164ea3c5362a3bcb295d212fd8af17aacac76c3
Santiago Bartesaghi <santib@hey.com>
2022-07-03 22:27:18 -0300

    Fix except when used everywhere (#29)

--- lib/ar2dto/model_config.rb
+++ lib/ar2dto/model_config.rb
@@ -2,22 +2,22 @@
 
 module AR2DTO
   class ModelConfig
-    attr_reader :model, :configs
+    attr_reader :model, :model_config
 
     def initialize(model)
       @model = model
     end
 
-    def setup_config(configs)
-      @configs = AR2DTO::Config.instance.as_json.merge(configs.as_json)
+    def setup_config(model_config)
+      @model_config ||= model_config
     end
 
     def active_model_compliance
-      @active_model_compliance ||= configs["active_model_compliance"]
+      @active_model_compliance ||= global_config.active_model_compliance
     end
 
     def except
-      @except ||= configs["except"]
+      @except ||= Array(global_config.except) | Array(model_config[:except])
     end
 
     def class_name
@@ -29,13 +29,17 @@ module AR2DTO
     end
 
     def namespaced_class_name
-      @namespaced_class_name ||= configs["class_name"] || model_name_replaced_suffix
+      @namespaced_class_name ||= model_config[:class_name] || model_name_replaced_suffix
     end
 
     private
 
     def model_name_replaced_suffix
-      "#{model.name.delete_suffix(configs["delete_suffix"].to_s)}#{configs["add_suffix"]}"
+      "#{model.name.delete_suffix(global_config.delete_suffix.to_s)}#{global_config.add_suffix}"
+    end
+
+    def global_config
+      AR2DTO::Config.instance
     end
   end
 end
