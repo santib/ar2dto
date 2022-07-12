04bed67a338114aa7d1d32580ea4cf9e4e34525c
Santiago Bartesaghi <santib@hey.com>
2022-07-12 18:45:21 -0300

    Add benchmarks

---
+++ benchmarks/initialization_and_method_calls.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require "bundler/inline"
+
+gemfile(true) do
+  source "https://rubygems.org"
+
+  git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+  gem "rails", github: "rails/rails"
+  gem "benchmark-ips"
+  gem "ar2dto", path: "../"
+  gem "sqlite3"
+  gem "byebug"
+end
+
+require "active_record"
+
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+
+require_relative "../spec/support/schema"
+require "zeitwerk"
+loader = Zeitwerk::Loader.new
+loader.inflector.inflect "person_dto" => "PersonDTO"
+loader.push_dir("spec/support/fixtures")
+loader.setup
+
+attributes = {
+  first_name: "Sandy",
+  last_name: "Doe",
+  email: "sandy@example.com",
+  birthday: Time.new(1995, 8, 25),
+  status: "pending"
+}
+user_methods = %i[full_name strange_name superman? myself]
+
+class UserPORO
+  attr_accessor(*(User.attribute_names + %i[full_name strange_name myself]))
+
+  def initialize(data = {})
+    data.each { |key, value| send("#{key}=", value) if respond_to?("#{key}=") }
+  end
+
+  def superman?
+    true
+  end
+end
+
+Benchmark.ips do |x|
+  user = nil
+
+  x.report("ActiveRecord") do
+    user = User.new(attributes)
+    user.first_name
+    user.last_name
+    user.email
+    user.birthday
+    user.status
+    user.full_name
+    user.strange_name
+    user.superman?
+    user.myself
+  end
+
+  x.report("PORO") do
+    user_poro = UserPORO.new(attributes.merge(user_methods.to_h { |method| [method, user.send(method.to_s)] }))
+    user_poro.first_name
+    user_poro.last_name
+    user_poro.email
+    user_poro.birthday
+    user_poro.status
+    user_poro.full_name
+    user_poro.strange_name
+    user_poro.superman?
+    user_poro.myself
+  end
+
+  x.report("AR2DTO::DTO") do
+    user_dto = user.to_dto(methods: user_methods)
+    user_dto.first_name
+    user_dto.last_name
+    user_dto.email
+    user_dto.birthday
+    user_dto.status
+    user_dto.full_name
+    user_dto.strange_name
+    user_dto.superman?
+    user_dto.myself
+  end
+
+  x.compare!
+end
+
+# Warming up --------------------------------------
+#         ActiveRecord     2.294k i/100ms
+#                 PORO     7.861k i/100ms
+#          AR2DTO::DTO     1.627k i/100ms
+# Calculating -------------------------------------
+#         ActiveRecord     23.635k (± 4.9%) i/s -    119.288k in   5.060888s
+#                 PORO     76.979k (± 6.3%) i/s -    385.189k in   5.024392s
+#          AR2DTO::DTO     12.459k (±14.0%) i/s -     61.826k in   5.069879s
+#
+# Comparison:
+#                 PORO:    76978.7 i/s
+#         ActiveRecord:    23635.1 i/s - 3.26x  (± 0.00) slower
+#          AR2DTO::DTO:    12459.1 i/s - 6.18x  (± 0.00) slower
